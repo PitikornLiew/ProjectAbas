557d0cfe38d129bae8c8b8f571cfb3dc5b62d186
PitikornLiew <116592696+PitikornLiew@users.noreply.github.com>
2023-08-16 04:51:19 +0700

    Initial commit

---
+++ src/Chachaphong/MainPage/Community/Community.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavbarCommunity from './Navbarcommunity'
+
+function Community() {
+  return (
+    <div>
+        <NavbarCommunity/>
+    </div>
+  )
+}
+
+export default Community
